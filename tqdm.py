39d62b8f59989dead168c0cf403fe2487c796ff5
techievivek <techievivek123@gmail.com>
2020-05-02 16:47:34 +0530

    added script to display a progress bar in terminal

--- tqdm.py
+++ tqdm.py
@@ -2,3 +2,10 @@ from tqdm import tqdm
 #This module can be used to display a progress bar in terminal.
 for i in tqdm(range(1000000)):
     pass
+#This can be populated with other features such as iterable=variable, desc='message', ascii=True, unit='KB', total=total_size
+'''Manual control of the tqdm'''
+pbar = tqdm(total=100)
+for i in range(10):
+    sleep(0.1)
+    pbar.update(10)
+pbar.close()
